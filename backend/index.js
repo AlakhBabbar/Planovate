8bdd898230d2c340c21246ab1d6338c4b809ce1f
unknown <alakh.babbar58@gmail.com>
2025-01-29 13:27:54 +0530

    localhost url frontend added for local development

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ connectDB();
 // Middleware
 app.use(cors(
     {
-        origin: ["https://planovate.vercel.app"],
+        origin: ["https://planovate.vercel.app", "http://localhost:3000"],
         methods: ["GET", "POST", "PUT", "DELETE"],
         credentials: true
     }
